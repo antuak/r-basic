ec280a94974ef72c698a17907bd123a1f8fde3e7
Antonio Ortega <libertyacademica@gmail.com>
2020-09-04 01:48:23 -0500

    Actualizacion de practicas

---
+++ Antuak/Practicas nivel 1/01. Como instalar y cargar librerías en R.r
@@ -0,0 +1,11 @@
+#Script que instala o actualiza una biblioteca en R
+install.packages("nombre_del_paquete", dep = TRUE)
+
+#Script que carga una biblioteca en R
+library(nombre_del_paquete)
+
+#Ejemplo:
+
+install.packages("tidyverse", dep = TRUE)
+
+library(tidyverse)
